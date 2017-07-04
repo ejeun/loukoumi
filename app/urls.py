20bca43458c9faf5e002787b49b533f9cfe05ba4
jeunlee <junniesu@gmail.com>
2017-07-04 18:45:35 -0400

    form accepts text and makes list

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<user_id>[0-9]+)/submit/$', views.submit, name='submit'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,10 @@ def index(request) :
     user = {'name': 'Jenny', 'id': 1}
     context = {'user': user}
     return render(request, 'app/index.html', context)
+
+def submit(request, user_id):
+    # user = userExists()
+    ids = request.POST['cusips']
+    idList = ids.split()
+    print('this is the POST from the request', idList)
+    return render(request, 'app/success.html')
